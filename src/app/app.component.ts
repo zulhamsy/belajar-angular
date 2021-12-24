37d5d06ab3dddd3af835d317ebc4f7763d982e0a
Zulham Syafrawi <official.zulham@gmail.com>
2021-12-24 12:34:51 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-app';
+  constructor() {
+    setTimeout(() => {
+      this.buttonText = 'Server: Power On';
+      this.buttonDisabled = false;
+    }, 2000);
+  }
+
+  onServerOn() {
+    this.serverStatus = 'Online';
+  }
+
+  onInputField(event: Event) {
+    this.displayText = (<HTMLInputElement>event.target).value;
+  }
+
+  resetNama() {
+    this.nama = '';
+  }
+
+  title = 'Root App';
+  buttonText = 'Server: Power Off';
+  buttonDisabled = true;
+  serverStatus = 'Offline';
+  displayText = 'Coba ganti';
+  nama = '';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './alert/success-alert/success-alert.component';
+import { WarningAlertComponent } from './alert/warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SuccessAlertComponent, WarningAlertComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
